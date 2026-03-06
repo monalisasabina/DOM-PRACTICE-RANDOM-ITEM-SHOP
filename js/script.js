7f3eefb6e556fde7b862ef20441ad6f1bce474b7
Monalisa Sabina <onyangomonalisa@hotmail.com>
2026-03-06 15:36:02 +0300

    added item description

--- js/script.js
+++ js/script.js
@@ -6,11 +6,23 @@ const cartItemsContainer = document.getElementById("cart-items");
 const totalAmountElement = document.getElementById("total-amount");
 let total=0;
 
-// Displaying all items
+// Displaying all items (items card)
 items.forEach(item =>{
   
-    // creating the list
+    // creating the list element
     const li =document.createElement("li");
+
+    //Creating image element
+    const image = document.createElement("img");
+    image.src = item.img;
+    image.alt = item.name;
+
+    // creating item name element
+    const name = document.createElement("h3");
+    name.textContent = item.name;
+
+
+
 
     
 
