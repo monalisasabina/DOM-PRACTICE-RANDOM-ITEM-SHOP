97860af93954dceba95a1ccfa228dea2e8d53cce
Monalisa Sabina <onyangomonalisa@hotmail.com>
2026-03-06 15:22:47 +0300

    created items file

---
+++ js/items.js
@@ -0,0 +1,24 @@
+// These are the items that will be displayed in the shop. You can add more items to this array as needed.
+
+export const items = [
+  { name: "Crochet Hat", price: 500, img: "https://source.unsplash.com/200x200/?hat" },
+  { name: "Hand Bag", price: 1200, img: "https://source.unsplash.com/200x200/?handbag" },
+  { name: "Wind Spinner", price: 300, img: "https://source.unsplash.com/200x200/?spinner" },
+  { name: "Vintage Mug", price: 400, img: "https://source.unsplash.com/200x200/?mug" },
+  { name: "Decorative Vase", price: 800, img: "https://source.unsplash.com/200x200/?vase" },
+  { name: "Retro Clock", price: 1500, img: "https://source.unsplash.com/200x200/?clock" },
+  { name: "Handmade Candle", price: 350, img: "https://source.unsplash.com/200x200/?candle" },
+  { name: "Mini Plant Pot", price: 250, img: "https://source.unsplash.com/200x200/?plant" },
+  { name: "Art Print", price: 1000, img: "https://source.unsplash.com/200x200/?art" },
+  { name: "Leather Wallet", price: 900, img: "https://source.unsplash.com/200x200/?wallet" },
+  { name: "Knitted Scarf", price: 600, img: "https://source.unsplash.com/200x200/?scarf" },
+  { name: "Wooden Key Holder", price: 400, img: "https://source.unsplash.com/200x200/?keyholder" },
+  { name: "Decorative Pillow", price: 700, img: "https://source.unsplash.com/200x200/?pillow" },
+  { name: "Ceramic Plate", price: 350, img: "https://source.unsplash.com/200x200/?ceramic" },
+  { name: "Mini Backpack", price: 1100, img: "https://source.unsplash.com/200x200/?backpack" },
+  { name: "Scented Soap", price: 200, img: "https://source.unsplash.com/200x200/?soap" },
+  { name: "Wall Hanging", price: 950, img: "https://source.unsplash.com/200x200/?wallhanging" },
+  { name: "Decorative Jar", price: 550, img: "https://source.unsplash.com/200x200/?jar" },
+  { name: "Canvas Tote", price: 650, img: "https://source.unsplash.com/200x200/?tote" },
+  { name: "Desk Organizer", price: 800, img: "https://source.unsplash.com/200x200/?organizer" },
+];
---
+++ js/script.js
@@ -0,0 +1,19 @@
+import { items } from "./items";
+
+// DOM Elements
+const itemsContainer = document.getElementById("the-items");
+const cartItemsContainer = document.getElementById("cart-items");
+const totalAmountElement = document.getElementById("total-amount");
+let total=0;
+
+// Displaying all items
+items.forEach(item =>{
+  
+    // creating the list
+    const li =document.createElement("li");
+
+    
+
+
+
+})
