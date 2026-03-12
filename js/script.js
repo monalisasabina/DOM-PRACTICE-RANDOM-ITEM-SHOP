1e7ea764ad476cac5894cdcdb27e8c1c634ce593
Monalisa Sabina <onyangomonalisa@hotmail.com>
2026-03-12 16:07:42 +0300

    focussing on the quantity problem

--- js/script.js
+++ js/script.js
@@ -7,40 +7,7 @@ const cartItemsContainer = document.getElementById("cart-items");
 const totalAmountElement = document.getElementById("total-amount");
 let total=0;
 
-
-// Add to cart function
-const addToCart = (item) => {
-    
-    // Creating cart list element
-    const cartList = document.createElement("li");
-    cartList.textContent = `${item.name} - KSH ${item.price}`;
-
-
-    // delete cart item button
-    const deleteItemCartButton = document.createElement("button");
-    deleteItemCartButton.textContent = "Remove";
-    deleteItemCartButton.addEventListener("click", () => {
-        cartList.remove();
-        total -= item.price;
-        totalAmountElement.textContent = total.toFixed(2);
-        })
-
-    //button goes inside the cart list item 
-    cartList.appendChild(deleteItemCartButton);
-
-    //Adding cart list to the cart container    
-    cartItemsContainer.appendChild(cartList);
-        
-
-    // Getting the total amount and updating it
-    total += item.price;
-    totalAmountElement.textContent = total.toFixed(2);
-
-        // toFixed()
-        //  Formats a number to a fixed number of decimal places
-}
-
-
+// ITEMS CARD
 // Displaying all items (items card)
 items.forEach(item =>{
   
@@ -80,43 +47,68 @@ items.forEach(item =>{
     quantity.type = "number";
     quantity.min = 0;
     quantity.value = currentQuantity;
+    
+    // New Price elememnt
+    const updatedPrice = document.createElement("p");
+      updatedPrice.textContent = `TOTAL Price: KSH 0`
 
-  
-    // new price based on quantity
-    const updatePrice = () => {
+    // Addlistener for the Input element
+    quantity.addEventListener("change", () => {
+        currentQuantity = parseInt(quantity.value);
         const newPriceCalculated = item.price * currentQuantity;
-        newPrice.textContent = `Price: KSH ${newPriceCalculated}`;
-    };
+        updatedPrice.textContent = `TOTAL Price: KSH ${newPriceCalculated}`;
+    })
 
-    // Update price when quantity changes
-    addQuantity.addEventListener("click", updatePrice);
-    removeQuantity.addEventListener("click", updatePrice);
- 
-    // new price element
-    const newPrice = document.createElement("p");
-    newPrice.textContent = `Price: KSH ${item.price}`;
 
     // ------------------------------------------------------------------------------------------------------------------------
 
+    // CART
+    // Add to cart function
+    const addToCart = (item, currentQuantity) => {
+    
+        // Creating cart list element
+        const cartList = document.createElement("li");
+        cartList.textContent = `${item.name} - KSH ${item.price*currentQuantity}`;
+
+        // delete cart item button
+        const deleteItemCartButton = document.createElement("button");
+        deleteItemCartButton.textContent = "Remove";
+        deleteItemCartButton.addEventListener("click", () => {
+            cartList.remove();
+            total -= item.price *currentQuantity;
+            totalAmountElement.textContent = total.toFixed(2);
+        })
+
+        //button goes inside the cart list item 
+        cartList.appendChild(deleteItemCartButton);
+
+        //Adding cart list to the cart container    
+        cartItemsContainer.appendChild(cartList);
+        
+
+        // Getting the total amount and updating it
+        total += item.price * currentQuantity;
+        totalAmountElement.textContent = total.toFixed(2);
+
+        // toFixed()
+        //  Formats a number to a fixed number of decimal places
+    }
 
     // Add cart button
     const addCartButton = document.createElement("button");
-       // name of the button
-    addCartButton.textContent = "Add to Cart";
-    addCartButton.addEventListener("click", () => addToCart(item));
+    addCartButton.textContent = "Add to Cart"; //name of the button
+    addCartButton.addEventListener("click", () => addToCart(item, currentQuantity)); // calling the add to cart function and passing the item and quantity as arguments
+
 
     // Building the  item card
     li.appendChild(name);
     li.appendChild(image);
     li.appendChild(description);
     li.appendChild(price);
-    li.appendChild(addCartButton);
     li.appendChild(quantityTitle);
-    li.appendChild(quantity);
-    li.appendChild(addQuantity);
-    li.appendChild(removeQuantity);
-    li.appendChild(newPrice);
-    
+    li.appendChild(quantity);  
+    li.appendChild(updatedPrice);
+    li.appendChild(addCartButton);
 
     // Adding the item card to the items container
     itemsContainer.appendChild(li);
